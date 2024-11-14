7175b932472ea8133c8cd9fed89d2d3b6094f927
Justin Davidson <76005901+JustinDavidsonStruxure@users.noreply.github.com>
2024-11-14 10:21:51 -0600

    Refactor App component to use local storage hooks for state management; move Dwight's info to a separate data file for better organization

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useState, useEffect } from 'react';
 import CvForm from './components/cv-form/cv-form.jsx';
 import CvPrint from './components/cv-print/cv-print.jsx';
 import styled from 'styled-components';
+import dwightInfo from './data/dwightsInfo';
+
+const exampleInfo = dwightInfo;
 
 const Container = styled.div`
   display: flex;
@@ -43,109 +46,43 @@ const Button = styled.button`
   }
 `;
 
-const dwightInfo = {
-  generalInfo: {
-    fullName: 'Dwight Schrute',
-    email: 'dwight.schrute@dundermifflin.com',
-    phone: '123-456-7890',
-    aboutMe:
-      '<p>I am a dedicated and hardworking Assistant to the Regional Manager at Dunder Mifflin. With a strong background in sales and a passion for beet farming, I strive to achieve excellence in all my endeavors. My goal is to become the Regional Manager and lead the Scranton branch to new heights.</p>',
-  },
-  educationList: [
-    {
-      school: 'Scranton High School',
-      degree: 'High School Diploma',
-      year: '1993',
-    },
-    {
-      school: 'Pennsylvania State University',
-      degree: 'Bachelor of Business Administration',
-      year: '1997',
-    },
-  ],
-  workList: [
-    {
-      company: 'Dunder Mifflin',
-      title: 'Assistant to the Regional Manager',
-      date: '2001-Present',
-      description: `
-      <ul>
-          <li>Consistently ranked as the top salesman in the Scranton branch, achieving the highest sales numbers for multiple consecutive years.</li>
-          <li>Awarded Employee of the Month several times for outstanding performance and dedication to the company.</li>
-          <li>Successfully managed office operations and supported the Regional Manager in various administrative tasks, contributing to the overall efficiency of the branch.</li>
-          <li>Developed and conducted sales training programs for new hires, improving their sales techniques and contributing to the overall success of the sales team.</li>
-          <li>Maintained strong relationships with key clients, resulting in high client retention rates and repeat business.</li>
-        </ul>
-      `,
-    },
-    {
-      company: 'Schrute Farms',
-      title: 'Owner',
-      date: '1997-Present',
-      description: ` 
-        <ul>
-          <li>Implemented sustainable and organic farming practices, resulting in high-quality beet production and increased farm profitability.</li>
-          <li>Developed and promoted Schrute Farms as a successful agrotourism destination, attracting visitors for farm tours, bed and breakfast stays, and beet-related activities.</li>
-          <li>Expanded the farm's operations by acquiring additional land and diversifying crop production, leading to increased revenue and business growth.</li>
-        </ul>
-      `,
-    },
-  ],
-  skillList: [
-    { skill: 'Sales' },
-    { skill: 'Leadership' },
-    { skill: 'Beet Farming' },
-    { skill: 'Martial Arts' },
-  ],
-};
+function useLocalStorageState(key, defaultValue) {
+  const [state, setState] = useState(() => {
+    const saved = localStorage.getItem(key);
+    const initialValue = JSON.parse(saved);
+    return initialValue || defaultValue;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(state));
+  }, [key, state]);
+
+  return [state, setState];
+}
 
 function App() {
-  const [generalInfo, setGeneralInfo] = useState(() => {
-    const saved = localStorage.getItem('generalInfo');
-    const initialValue = JSON.parse(saved);
-    return initialValue || dwightInfo.generalInfo;
-  });
+  const [generalInfo, setGeneralInfo] = useLocalStorageState(
+    'generalInfo',
+    exampleInfo.generalInfo
+  );
+  const [educationList, setEducationList] = useLocalStorageState(
+    'educationList',
+    exampleInfo.educationList
+  );
+  const [workList, setWorkList] = useLocalStorageState(
+    'workList',
+    exampleInfo.workList
+  );
+  const [skillList, setSkillList] = useLocalStorageState(
+    'skillList',
+    exampleInfo.skillList
+  );
 
-  const [educationList, setEducationList] = useState(() => {
-    const saved = localStorage.getItem('educationList');
-    const initialValue = JSON.parse(saved);
-    return initialValue || dwightInfo.educationList;
-  });
-
-  const [workList, setWorkList] = useState(() => {
-    const saved = localStorage.getItem('workList');
-    const initialValue = JSON.parse(saved);
-    return initialValue || dwightInfo.workList;
-  });
-
-  const [skillList, setSkillList] = useState(() => {
-    const saved = localStorage.getItem('skillList');
-    const initialValue = JSON.parse(saved);
-    return initialValue || dwightInfo.skillList;
-  });
-
-  // Save state to local storage whenever it changes
-  useEffect(() => {
-    localStorage.setItem('generalInfo', JSON.stringify(generalInfo));
-  }, [generalInfo]);
-
-  useEffect(() => {
-    localStorage.setItem('educationList', JSON.stringify(educationList));
-  }, [educationList]);
-
-  useEffect(() => {
-    localStorage.setItem('workList', JSON.stringify(workList));
-  }, [workList]);
-
-  useEffect(() => {
-    localStorage.setItem('skillList', JSON.stringify(skillList));
-  }, [skillList]);
-
-  const setDwightInfo = () => {
-    setGeneralInfo(dwightInfo.generalInfo);
-    setEducationList(dwightInfo.educationList);
-    setWorkList(dwightInfo.workList);
-    setSkillList(dwightInfo.skillList);
+  const setExampleInfo = () => {
+    setGeneralInfo(exampleInfo.generalInfo);
+    setEducationList(exampleInfo.educationList);
+    setWorkList(exampleInfo.workList);
+    setSkillList(exampleInfo.skillList);
   };
 
   const clearInfo = () => {
@@ -163,7 +100,7 @@ function App() {
   return (
     <Container>
       <FormWrapper>
-        <Button onClick={setDwightInfo}>Example Resume</Button>
+        <Button onClick={setExampleInfo}>Example Resume</Button>
         <Button onClick={clearInfo}>Clear Resume</Button>
         <CvForm
           generalInfo={generalInfo}
---
+++ src/data/dwightsInfo.js
@@ -0,0 +1,57 @@
+const dwightInfo = {
+  generalInfo: {
+    fullName: 'Dwight Schrute',
+    email: 'dwight.schrute@dundermifflin.com',
+    phone: '123-456-7890',
+    aboutMe:
+      '<p>I am a dedicated and hardworking Assistant to the Regional Manager at Dunder Mifflin. With a strong background in sales and a passion for beet farming, I strive to achieve excellence in all my endeavors. My goal is to become the Regional Manager and lead the Scranton branch to new heights.</p>',
+  },
+  educationList: [
+    {
+      school: 'Scranton High School',
+      degree: 'High School Diploma',
+      year: '1993',
+    },
+    {
+      school: 'Pennsylvania State University',
+      degree: 'Bachelor of Business Administration',
+      year: '1997',
+    },
+  ],
+  workList: [
+    {
+      company: 'Dunder Mifflin',
+      title: 'Assistant to the Regional Manager',
+      date: '2001-Present',
+      description: `
+      <ul>
+          <li>Consistently ranked as the top salesman in the Scranton branch, achieving the highest sales numbers for multiple consecutive years.</li>
+          <li>Awarded Employee of the Month several times for outstanding performance and dedication to the company.</li>
+          <li>Successfully managed office operations and supported the Regional Manager in various administrative tasks, contributing to the overall efficiency of the branch.</li>
+          <li>Developed and conducted sales training programs for new hires, improving their sales techniques and contributing to the overall success of the sales team.</li>
+          <li>Maintained strong relationships with key clients, resulting in high client retention rates and repeat business.</li>
+        </ul>
+      `,
+    },
+    {
+      company: 'Schrute Farms',
+      title: 'Owner',
+      date: '1997-Present',
+      description: ` 
+        <ul>
+          <li>Implemented sustainable and organic farming practices, resulting in high-quality beet production and increased farm profitability.</li>
+          <li>Developed and promoted Schrute Farms as a successful agrotourism destination, attracting visitors for farm tours, bed and breakfast stays, and beet-related activities.</li>
+          <li>Expanded the farm's operations by acquiring additional land and diversifying crop production, leading to increased revenue and business growth.</li>
+        </ul>
+      `,
+    },
+  ],
+  skillList: [
+    { skill: 'Sales' },
+    { skill: 'Leadership' },
+    { skill: 'Beet Farming' },
+    { skill: 'Martial Arts' },
+  ],
+};
+
+export default dwightInfo;
