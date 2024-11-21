def7166bbe49c9f3a3ca4a151bd44813ddc440b3
Justin Davidson <76005901+JustinDavidsonStruxure@users.noreply.github.com>
2024-11-21 16:23:14 -0600

    Implement sortable functionality for education, skills, and work sections in CV form

--- src/components/cv-form/cv-form.jsx
+++ src/components/cv-form/cv-form.jsx
@@ -1,4 +1,6 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
+
 import styled from 'styled-components';
 import GeneralInfoForm from './general-info-form';
 import EducationForm from './education-form';
@@ -8,6 +10,11 @@ import { SectionWrapper } from './CommonStyles';
 import feather from 'feather-icons';
 import { useEffect } from 'react';
 import Sortable from './sortable';
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
 
 const Header = styled.h2`
   font-family: 'Roboto', sans-serif;
@@ -24,6 +31,15 @@ const Icon = styled.i`
   color: #c656fe;
 `;
 
+const Button = styled.button`
+  color: #c656fe;
+  border: none;
+  border-radius: 4px;
+  padding: 5px;
+  height: 15px;
+  background-color: transparent;å
+`;
+
 function CvForm({
   generalInfo,
   setGeneralInfo,
@@ -37,55 +53,89 @@ function CvForm({
   useEffect(() => {
     feather.replace();
   }, []);
+
+  const [isEducationSortable, setIsEducationSortable] = useState(false);
+  const [isSkillsSortable, setIsSkillsSortable] = useState(false);
+  const [isWorkSortable, setIsWorkSortable] = useState(false);
+
+  const toggleEducationSortable = () => {
+    setIsEducationSortable(!isEducationSortable);
+  };
+
+  const toggleSkillsSortable = () => {
+    setIsSkillsSortable(!isSkillsSortable);
+  };
+
+  const toggleWorkSortable = () => {
+    setIsWorkSortable(!isWorkSortable);
+  };
+
   return (
     <>
       <SectionWrapper>
-        <Header>
-          {' '}
-          <Icon data-feather="user"></Icon>
-          Personal Information
-        </Header>
+        <HeaderWrapper>
+          <Header>
+            {' '}
+            <Icon data-feather="user"></Icon>
+            Personal Information
+          </Header>
+        </HeaderWrapper>
         <GeneralInfoForm
           generalInfo={generalInfo}
           setGeneralInfo={setGeneralInfo}
         />
       </SectionWrapper>
       <SectionWrapper>
-        <Header>
-          {' '}
-          <Icon data-feather="book-open"></Icon>
-          Education
-        </Header>
-        <EducationForm
-          educationList={educationList}
-          setEducationList={setEducationList}
-        />
-      </SectionWrapper>
-      <SectionWrapper>
-        <Header>
-          {' '}
-          <Icon data-feather="award"></Icon>
-          Skills
-        </Header>
-        <Skills skillList={skillList} setSkillList={setSkillList} />
-      </SectionWrapper>
-      <SectionWrapper>
-        <Header>
-          {' '}
-          <Icon data-feather="briefcase"></Icon>
-          Work Experience
-        </Header>
-        <WorkForm workList={workList} setWorkList={setWorkList} />
-      </SectionWrapper>
+        <HeaderWrapper>
+          <Header>
+            <Icon data-feather="book"></Icon>
+            Education
+          </Header>
+          <Button type="button" onClick={toggleEducationSortable}>
+            {isEducationSortable ? 'Done' : 'Sort'}
+          </Button>
+        </HeaderWrapper>
 
-      <SectionWrapper>
-        <Sortable list={workList} setList={setWorkList} />
+        {isEducationSortable ? (
+          <Sortable list={educationList} setList={setEducationList} />
+        ) : (
+          <EducationForm
+            educationList={educationList}
+            setEducationList={setEducationList}
+          />
+        )}
       </SectionWrapper>
       <SectionWrapper>
-        <Sortable list={skillList} setList={setSkillList} />
+        <HeaderWrapper>
+          <Header>
+            <Icon data-feather="award"></Icon>
+            Skills
+          </Header>
+          <Button type="button" onClick={toggleSkillsSortable}>
+            {isSkillsSortable ? 'Done' : 'Sort'}
+          </Button>
+        </HeaderWrapper>
+        {isSkillsSortable ? (
+          <Sortable list={skillList} setList={setSkillList} />
+        ) : (
+          <Skills skillList={skillList} setSkillList={setSkillList} />
+        )}
       </SectionWrapper>
       <SectionWrapper>
-        <Sortable list={educationList} setList={setEducationList} />
+        <HeaderWrapper>
+          <Header>
+            <Icon data-feather="briefcase"></Icon>
+            Work Experience
+          </Header>
+          <Button type="button" onClick={toggleWorkSortable}>
+            {isWorkSortable ? 'Done' : 'Sort'}
+          </Button>
+        </HeaderWrapper>
+        {isWorkSortable ? (
+          <Sortable list={workList} setList={setWorkList} />
+        ) : (
+          <WorkForm workList={workList} setWorkList={setWorkList} />
+        )}
       </SectionWrapper>
     </>
   );
