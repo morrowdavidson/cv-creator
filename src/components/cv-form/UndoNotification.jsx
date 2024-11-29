2940668cf9bac9941cf4a92111cf7f576e13ca5b
Justin Davidson <76005901+JustinDavidsonStruxure@users.noreply.github.com>
2024-11-29 17:29:27 -0600

    Add favicon and update document title in index.html

--- src/components/cv-form/UndoNotification.jsx
+++ src/components/cv-form/UndoNotification.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { UndoMessage, UndoTimer, IconButton } from './CommonStyles';
 import { RotateCcw } from 'react-feather';
